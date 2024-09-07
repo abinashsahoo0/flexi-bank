1f5b8728888e60b6bb1b8a548320da20addb20b1
abinashsahoo0 <sahooabinash801@gmail.com>
2024-09-07 12:14:33 +0530

    Banking

--- backend/src/main/java/com/jtbank/backend/component/AuditingImpl.java
+++ backend/src/main/java/com/jtbank/backend/component/AuditingImpl.java
@@ -1,14 +1,15 @@
 package com.jtbank.backend.component;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class AuditingImpl implements AuditorAware<String> {
+
     @Override
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
